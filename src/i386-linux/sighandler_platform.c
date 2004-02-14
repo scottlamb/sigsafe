8f607a8b4f656fad1284769a17d926b5afedffc5
slamb <slamb@slamb.org>
2004-02-14 06:58:53 +0000

    Start of i386-linux implementation

---
+++ src/i386-linux/sighandler_platform.c
@@ -0,0 +1,21 @@
+/* $Id: sighandler_platform.c$ */
+
+#define _GNU_SOURCE
+#define ORG_SLAMB_SIGSAFE_INTERNAL
+#include <sigsafe.h>
+#include <ucontext.h>
+#include <signal.h>
+#include <stdlib.h>
+
+void sighandler_for_platform(ucontext_t *ctx) {
+    struct sigsafe_syscall *s;
+    void *eip;
+    eip = (void*) ctx->uc_mcontext.gregs[REG_EIP];
+    for (s = sigsafe_syscalls; s->address != NULL; s++) {
+        if (s->minjmp <= eip && eip <= s->maxjmp) {
+            ctx->uc_mcontext.gregs[REG_EIP] = (int) s->jmpto;
+            setcontext(ctx);
+            abort();
+        }
+    }
+}
