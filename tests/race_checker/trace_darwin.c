b1965954f1de992bca97dcd81b28d31ae6307bb6
slamb <slamb@slamb.org>
2004-02-16 00:48:33 +0000

    Vain effort to make ptrace() work under Darwin.

--- tests/race_checker/trace_darwin.c
+++ tests/race_checker/trace_darwin.c
@@ -3,19 +3,23 @@
 #include <sys/types.h>
 #include <sys/ptrace.h>
 #include <stdlib.h>
+#include <errno.h>
 #include "race_checker.h"
 
 void trace_attach(pid_t pid) {
+    errno = 0;
     error_wrap(ptrace(PT_ATTACH, pid, NULL, NULL),
                "ptrace(PT_ATTACH, ...)", ERRNO);
 }
 
 void trace_step(pid_t pid, int signum) {
-    error_wrap(ptrace(PT_STEP, pid, NULL, (void*) signum),
+    errno = 0;
+    error_wrap(ptrace(PT_STEP, pid, (caddr_t) 1, (void*) signum),
                "ptrace(PT_STEP, ...)", ERRNO);
 }
 
 void trace_detach(pid_t pid, int signum) {
-    error_wrap(ptrace(PT_DETACH, pid, NULL, (void*) signum),
+    errno = 0;
+    error_wrap(ptrace(PT_DETACH, pid, (caddr_t) 1, (void*) signum),
                "ptrace(PT_DETACH, ...)", ERRNO);
 }
