7c5f388b61f051f3e7b12daa8a4018d09ae73f3b
slamb <slamb@slamb.org>
2004-05-01 23:08:13 +0000

    New test - I'm suspicious we might be discarding a valid return from read() under some platforms / circumstances, and this test should trigger them if so.

---
+++ tests/test_pipe_bytecount.c
@@ -0,0 +1,99 @@
+/** @file
+ * Tests if signal delivery can cause us to lose track of received bytes.
+ * The read documentation in SUSv3 includes:
+ *
+ *      "If a read() is interrupted by a signal after it has successfully read
+ *      some data, it shall return the number of bytes read."
+ *
+ * In this case, it seems reasonable to think that the signal handler would be
+ * called first. If it jumps (as we do) rather than returning to the kernel,
+ * this byte count may be discarded.
+ *
+ * If I find a platform on which this test fails (loops forever), I'll have to
+ * turn off SA_RESTART and ensure we don't ever jump from the actual system
+ * call instruction, just immediately before it. I may do that anyway, for
+ * safety.
+ *
+ * @legal
+ * Copyright &copy 2004 &lt;slamb@slamb.org&gt;.
+ * This file is part of sigsafe, which is released under the MIT license.
+ * @version     $Id$
+ * @author      Scott Lamb &lt;slamb@slamb.org&gt;
+ */
+
+#include <limits.h>
+#include <unistd.h>
+#include <setjmp.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <sys/select.h>
+#include <errno.h>
+#include <sigsafe.h>
+#include <stdio.h>
+
+#define min(a,b) ((a)<(b)?(a):(b))
+
+enum {
+    READ = 0,
+    WRITE
+} PipeHalf;
+
+/* Write a lot of bytes */
+#define BYTES_TO_TRANSFER 4294967295u
+
+/* ...in groups of more than PIPE_BUF to maximize the chances of something
+ * interesting happening. */
+#define SINGLE_TRANSFER (16*PIPE_BUF)
+
+void sigusr1handler(int signo, siginfo_t *si, ucontext_t *ctx, intptr_t baton) {
+    write(1, ".", 1);
+}
+
+int main(void) {
+    int parent_pid;
+    int mypipe[2];
+    char buffer[SINGLE_TRANSFER];
+    size_t total_sent = 0, total_rcvd = 0;
+
+    sigsafe_install_handler(SIGUSR1, &sigusr1handler);
+    sigsafe_install_tsd(0, NULL);
+
+    parent_pid = getpid();
+    pipe(mypipe);
+
+    if (fork() == 0) { /* writer */
+        while (total_sent < BYTES_TO_TRANSFER) {
+            ssize_t retval;
+            retval = write(mypipe[WRITE], buffer,
+                           min(SINGLE_TRANSFER, BYTES_TO_TRANSFER - total_sent));
+            assert(retval > 0);
+            total_sent += retval;
+        }
+        return 0;
+    }
+
+    if (fork() == 0) { /* signaler */
+        while (1) {
+            /*usleep(50);*/
+            kill(parent_pid, SIGUSR1);
+        }
+        /* not reached */
+    }
+
+    /* reader */
+    while (total_rcvd < BYTES_TO_TRANSFER) {
+        ssize_t retval;
+        size_t this_transfer = min(SINGLE_TRANSFER, BYTES_TO_TRANSFER - total_rcvd);
+        retval = sigsafe_read(mypipe[READ], buffer, this_transfer);
+        if (retval == -EINTR) {
+            sigsafe_clear_received();
+            continue;
+        }
+        write(1, "#", 1);
+        if (retval != this_transfer)
+            printf("\nretval: %d\n", retval);
+        assert(retval > 0);
+        total_rcvd += retval;
+    }
+    return 0;
+}
