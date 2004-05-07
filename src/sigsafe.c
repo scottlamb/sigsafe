c5265a7432711f5edcc16898b58abd4627d4ee49
slamb <slamb@slamb.org>
2004-05-07 16:34:55 +0000

    Single-threaded/multi-threaded split. Only tested so far under Darwin/ppc (and lightly, at that).
    I'm optimistic that a single-threaded FreeBSD version will work.
    
    Also got rid of the separate builds for debug/optimized. It didn't make a
    difference for the performance-sensitive code, so I'll just stick with debug.
    There's a flag still for the debug defines (that write [S] and [J] on signal
    receipt). But I expect that once a platform is working, it will just be
    toggled off. No need for separate builds.

--- src/sigsafe.c
+++ src/sigsafe.c
@@ -14,8 +14,14 @@
 #include <stdlib.h>
 #include <errno.h>
 
+#ifdef _THREAD_SAFE
 pthread_key_t sigsafe_key;
 static pthread_once_t sigsafe_once = PTHREAD_ONCE_INIT;
+#else
+struct sigsafe_tsd *sigsafe_data;
+static int sigsafe_inited;
+#endif
+
 sigsafe_user_handler_t user_handlers[SIGSAFE_SIGMAX];
 
 #define SYSCALL(name, args) \
@@ -35,33 +41,43 @@ struct sigsafe_syscall sigsafe_syscalls[] = {
 #undef SYSCALL
 
 static void sighandler(int signum, siginfo_t *siginfo, ucontext_t *ctx) {
-    struct sigsafe_tsd *tsd = pthread_getspecific(sigsafe_key);
+#ifdef _THREAD_SAFE
+    struct sigsafe_tsd *sigsafe_data = pthread_getspecific(sigsafe_key);
+#endif
     assert(0 < signum && signum <= SIGSAFE_SIGMAX);
 #ifdef ORG_SLAMB_SIGSAFE_DEBUG_SIGNAL
     write(2, "[S]", 3);
 #endif
-    if (tsd != NULL) {
+    if (sigsafe_data != NULL) {
         if (user_handlers[signum - 1] != NULL) {
-            user_handlers[signum - 1](signum, siginfo, ctx, tsd->user_data);
+            user_handlers[signum - 1](signum, siginfo, ctx, sigsafe_data->user_data);
         }
-        tsd->signal_received = 1;
+        sigsafe_data->signal_received = 1;
         sighandler_for_platform(ctx);
     }
 }
 
+#ifdef _THREAD_SAFE
 static void tsd_destructor(void* tsd_v) {
-    struct sigsafe_tsd *tsd = (struct sigsafe_tsd*) tsd_v;
-    if (tsd->destructor != NULL) {
-        tsd->destructor(tsd->user_data);
+    struct sigsafe_tsd *sigsafe_data = (struct sigsafe_tsd*) tsd_v;
+#else
+static void tsd_destructor(void) {
+#endif
+    if (sigsafe_data->destructor != NULL) {
+        sigsafe_data->destructor(sigsafe_data->user_data);
     }
-    free(tsd);
+    free(sigsafe_data);
 }
 
 static void sigsafe_init(void) {
     /* "volatile" so our seemingly-useless references aren't optimized away. */
     volatile void *fp;
 
+#ifdef _THREAD_SAFE
     pthread_key_create(&sigsafe_key, &tsd_destructor);
+#else
+    atexit(&tsd_destructor);
+#endif
 
     /*
      * XXX
@@ -80,7 +96,14 @@ int sigsafe_install_handler(int signum, sigsafe_user_handler_t handler) {
     struct sigaction sa;
 
     assert(0 < signum && signum <= SIGSAFE_SIGMAX);
+#ifdef _THREAD_SAFE
     pthread_once(&sigsafe_once, &sigsafe_init);
+#else
+    if (!sigsafe_inited) {
+        sigsafe_inited = 1;
+        sigsafe_init();
+    }
+#endif
     user_handlers[signum - 1] = handler;
     sa.sa_sigaction = (void*) &sighandler;
     sa.sa_flags = SA_RESTART | SA_SIGINFO;
@@ -97,34 +120,42 @@ int sigsafe_install_handler(int signum, sigsafe_user_handler_t handler) {
 }
 
 int sigsafe_install_tsd(intptr_t user_data, void (*destructor)(intptr_t)) {
-    struct sigsafe_tsd *tsd = NULL;
+#ifdef _THREAD_SAFE
+    struct sigsafe_tsd *sigsafe_data = NULL;
     int retval;
 
     assert(pthread_getspecific(sigsafe_key) == NULL);
+#else
+    assert(sigsafe_data == NULL);
+#endif
 
-    tsd = (struct sigsafe_tsd*) malloc(sizeof(struct sigsafe_tsd));
-    if (tsd == NULL) {
+    sigsafe_data = (struct sigsafe_tsd*) malloc(sizeof(struct sigsafe_tsd));
+    if (sigsafe_data == NULL) {
         return -ENOMEM;
     }
 
-    tsd->signal_received = 0;
-    tsd->user_data = user_data;
-    tsd->destructor = destructor;
+    sigsafe_data->signal_received = 0;
+    sigsafe_data->user_data = user_data;
+    sigsafe_data->destructor = destructor;
 
-    retval = pthread_setspecific(sigsafe_key, tsd);
+#ifdef _THREAD_SAFE
+    retval = pthread_setspecific(sigsafe_key, sigsafe_data);
     if (retval != 0) {
-        free(tsd);
+        free(sigsafe_data);
         return -retval;
     }
+#endif
 
     return 0;
 }
 
 intptr_t sigsafe_clear_received(void) {
-    struct sigsafe_tsd *tsd;
+#ifdef _THREAD_SAFE
+    struct sigsafe_tsd *sigsafe_data;
 
-    tsd = (struct sigsafe_tsd*) pthread_getspecific(sigsafe_key);
-    assert(tsd != NULL);
-    tsd->signal_received = 0;
-    return tsd->user_data;
+    sigsafe_data = (struct sigsafe_tsd*) pthread_getspecific(sigsafe_key);
+#endif
+    assert(sigsafe_data != NULL);
+    sigsafe_data->signal_received = 0;
+    return sigsafe_data->user_data;
 }
